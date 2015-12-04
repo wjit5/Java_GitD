2da800c2c9d201165bc2cabf0d7cb39ecb2d0761
wjit5@foxmail.com <wjit5@foxmail.com>
2015-12-04 14:29:59 +0800

    second commit:add something in plan_info

--- src/com/wjit5/main/entity/plan_info.java
+++ src/com/wjit5/main/entity/plan_info.java
@@ -4,5 +4,11 @@ public class plan_info {
 	Integer id;
 	String plan_code;
 	String plan_name;
+	String plan_enname;
+	String plan_type;
 	String plan_price;
+	String flag;
+	String promise;
+	String count;
+
 }
