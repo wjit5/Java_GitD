82d414d8561059157a7ae07192462a1b6cd8b3b1
wjit5 <wjit5@foxmail.com>
2015-12-04 15:54:56 +0800

    add users

---
+++ src/com/main/login/entity/Users.java
@@ -0,0 +1,50 @@
+package com.main.login.entity;
+
+public class Users {
+	Integer id;
+	String user_code;
+	String user_name;
+	String user_password;
+	String user_state;
+	String user_flag;
+	
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUser_code() {
+		return user_code;
+	}
+	public void setUser_code(String user_code) {
+		this.user_code = user_code;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getUser_password() {
+		return user_password;
+	}
+	public void setUser_password(String user_password) {
+		this.user_password = user_password;
+	}
+	public String getUser_state() {
+		return user_state;
+	}
+	public void setUser_state(String user_state) {
+		this.user_state = user_state;
+	}
+	public String getUser_flag() {
+		return user_flag;
+	}
+	public void setUser_flag(String user_flag) {
+		this.user_flag = user_flag;
+	}
+	
+}
